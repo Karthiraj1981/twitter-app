a131b4a4c567ea2b02e020ffb7bdd8039ea27eb1
Karthiraj <Karthiraj@gmail.com>
2020-08-27 23:20:42 +0530

    First commit - twitter application

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'ZAjS26skmRnz1l8Q7K7h7Go7y'
+CONSUMER_SECRET = 'j4SF6KuFO8rPD2f4u1qRETAFNmYd38GuF7LrhkvQGzjhjSDuRV'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
